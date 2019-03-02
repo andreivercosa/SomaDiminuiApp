e0138f78eb79704c24830442d167d708c8d90eec
andreivercosa <andreivercosa@gmail.com>
2019-03-01 20:21:18 -0400

    v1

---
+++ SomaDimunuiApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SomaDimunuiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int valor = 0;
+
+        void aumenta(object sender, EventArgs e)
+        {
+            valor++;
+            texto.Text =  valor.ToString();
+        }
+
+        void diminui(object sender, EventArgs e)
+        {
+            valor--;
+            texto.Text = valor.ToString();
+        }
+    }
+}
